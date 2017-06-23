351ebad401e0e1ef046bd48aeb652b4bd2b1cc6a
Florian Eckert <fe@dev.tdt.de>
2017-06-23 09:07:57 +0200

    oh-my-zsh-custom: source bash aliases

--- oh-my-zsh-custom/alias.zsh
+++ oh-my-zsh-custom/alias.zsh
@@ -1,2 +1,3 @@
-alias git-config-private='git config user.email Eckert.Florian@googlemail.com'
-alias git-config-work='git config user.email fe@dev.tdt.de'
+if [ -f $HOME/.bash_aliases ]; then
+	. ~/.bash_aliases
+fi
