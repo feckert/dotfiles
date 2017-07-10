9815efa9e787121c9ed01a40f17b554f0b8dcc6d
Florian Eckert <fe@dev.tdt.de>
2017-07-10 09:42:42 +0200

    irssi: remove irssi start/stop tmux script
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- bin/irssi-stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if tmux has-session -t 'irssi' 2>/dev/null; then
-	tmux kill-session -t irssi 2>/dev/null
-	echo "Irssi has bin stopped"
-else
-	echo "Irssi is not running"
-	echo "To start it use: ./irssi-start.sh"
-fi
