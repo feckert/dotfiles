43749824e8b0df3141cc04e2facfde9cf307e19d
Florian Eckert <fe@dev.tdt.de>
2017-10-01 12:39:06 +0200

    git-config-*: fix comment
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- bin/git-config-global-email-private.sh
+++ bin/git-config-global-email-private.sh
@@ -5,5 +5,3 @@ git config --global sendemail.smtpserver smtp.googlemail.com
 git config --global sendemail.smtpserverport 587
 git config --global sendemail.smtpuser Eckert.Florian@googlemail.com
 git config --global sendemail.smtpencryption tls
-
-#sendemail.smtpsslcertpath=/home/feckert/maildevtdtde.crt
--- bin/git-config-global-email-work.sh
+++ bin/git-config-global-email-work.sh
@@ -5,5 +5,3 @@ git config --global sendemail.smtpserver mail.dev.tdt.de
 git config --global sendemail.smtpserverport 25
 git config --global sendemail.smtpuser fe@dev.tdt.de
 git config --global sendemail.smtpencryption tls
-
-#sendemail.smtpsslcertpath=/home/feckert/maildevtdtde.crt
