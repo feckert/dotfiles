817a74c7e332a262b9fa4bbecdc178144fe1ab76
Oliver Kraitschy <okraits@users.noreply.github.com>
2017-11-03 10:02:02 +0100

    Improve sshfsDir.sh: add option -u to unmount a directory

--- bin/sshfsDir.sh
+++ bin/sshfsDir.sh
@@ -1,30 +1,47 @@
 #!/bin/sh
 
-IP=$1
-USER=$2
-DIR=ssh_${IP}
+if [ "$1" != "-u" ]; then
 
-if [ -z "${IP}" ] ; then
-	echo "Please append IP"; exit 1
-fi
+	IP=$1
+	USER=$2
+	DIR=ssh_${IP}
 
-if [ -z "${USER}" ] ; then
-        echo "Please append user name"; exit 1
-fi
+	if [ -z "${IP}" ] ; then
+		echo "Please append IP"; exit 1
+	fi
 
-# Check if directory exist
-if [ ! -d "~/mnt/${DIR}" ] ; then
-	echo "execute -> mkdir -p ~/mnt/${DIR}"
-	mkdir -p ~/mnt/${DIR}
-else 
-	echo "Directory already set"
-fi
+	if [ -z "${USER}" ] ; then
+					echo "Please append user name"; exit 1
+	fi
 
-# Check if directory is empty
-if [ ! "$(ls -A ~/mnt/$DIR)" ]; then 
-	echo "execute -> sshfs ${USER}@${IP}:/ ~/mnt/${DIR} -o idmap=user -o uid=$(id -u) -o gid=$(id -g)"
-	sshfs ${USER}@${IP}:/ ~/mnt/${DIR} -o idmap=user -o uid=$(id -u) -o gid=$(id -g)
+	# Check if directory exist
+	if [ ! -d "~/mnt/${DIR}" ] ; then
+		echo "execute -> mkdir -p ~/mnt/${DIR}"
+		mkdir -p ~/mnt/${DIR}
+	else 
+		echo "Directory already set"
+	fi
+
+	# Check if directory is empty
+	if [ ! "$(ls -A ~/mnt/$DIR)" ]; then 
+		echo "execute -> sshfs ${USER}@${IP}:/ ~/mnt/${DIR} -o idmap=user -o uid=$(id -u) -o gid=$(id -g)"
+		sshfs ${USER}@${IP}:/ ~/mnt/${DIR} -o idmap=user -o uid=$(id -u) -o gid=$(id -g)
+	else
+		echo "Directory not empty"
+	fi
+
 else
-	echo "Directory not empty"
-fi
 
+	DIR=$2
+
+	if [ -z "${DIR}" ] ; then
+		echo "Please append directory"; exit 1
+	fi
+
+	if [ "$(cat /etc/mtab | grep -w ${DIR})" ]; then
+		echo "execute -> fusermount -u ${DIR}"
+		fusermount -u ${DIR}
+	else
+		echo "Directory not mounted"
+	fi
+fi
