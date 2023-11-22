23da9c424c9598b150fdda542856568f09fc3fd5
Florian Eckert <fe@dev.tdt.de>
2023-11-22 16:58:57 +0100

    nvim: add whitspaces and 80 char boundary
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.listchars = {
+  space = ".",
+  tab = "» ",
+  trail = ".",
+  extends = ">",
+  precedes = "<",
+  eol = "↵",
+}
+--stab:\|.,trail:.,extends:>,precedes:<
+vim.opt.list = true
+
+-- Set color colum at 80 char boundary
+vim.opt.colorcolumn = "80"
