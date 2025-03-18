885cfd8487a75f820963626414a04eb5ce52a102
Florian Eckert <fe@dev.tdt.de>
2025-03-18 13:31:25 +0100

    nvim: fix spaces vs. tabs
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.opt.tabstop = 8 -- Number of spaces that a <Tab> counts for
+vim.opt.shiftwidth = 8 -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = false -- Use spaces instead of tabs
